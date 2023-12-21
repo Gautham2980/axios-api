7d8b510d0ba492826e9f8061772fb139f59327fc
Gautham2980 <gautham4816@gmail.com>
2023-12-21 12:26:49 +0530

    axios

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Api from './Card';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='row row-cols-lg-3 mt-5'>
+      <Api />
+
+    </div>
+      
+    </div>
+  );
+}
+
+export default App;
